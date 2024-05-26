3687435474de4426fe9e492276c88016797c440c
Andres Vaquero <web.andresvaquero@gmail.com>
2024-05-26 23:07:01 +1000

    rearranging exports, adding fieldsByType to exports

---
+++ src/exports.js
@@ -0,0 +1,81 @@
+import '@ungap/custom-elements';
+/**
+ * Form.
+ */
+export { default as FormComponent } from './components/form/form.js';
+export { default as SubmitButton } from './fields/submitButton/submitButton.js';
+export { default as FormTitle } from './components/form/components/formTitle/formTitle.js';
+
+/**
+ * Field.
+ */
+export { default as Field } from './fields/field/field.js';
+export { default as FieldErrors } from './fields/field/components/fieldErrors/fieldErrors.js';
+export { default as FieldInput } from './fields/field/components/fieldInput/fieldInput.js';
+export { default as FieldLabel } from './fields/field/components/fieldLabel/fieldLabel.js';
+export { default as FieldDescription } from './fields/field/components/fieldDescription/fieldDescription.js';
+export { default as FieldFootnote } from './fields/field/components/fieldFootnote/fieldFootnote.js';
+export { default as FieldInputMask } from './fields/field/components/fieldInputMask/fieldInputMask.js';
+
+/**
+ * Text Fields.
+ */
+export { default as TextField } from './fields/textField/textField.js';
+export { default as PasswordField } from './fields/passwordField/passwordField.js';
+export { default as TextAreaField } from './fields/textAreaField/textAreaField.js';
+export { default as NumberField } from './fields/numberField/numberField.js';
+export { default as ColorField } from './fields/colorField/colorField.js';
+export { default as EmailField } from './fields/emailField/emailField.js';
+export { default as UrlField } from './fields/urlField/urlField.js';
+export { default as TelField } from './fields/telField/telField.js';
+export { default as RangeField } from './fields/rangeField/rangeField.js';
+export { default as MonthField } from './fields/monthField/monthField.js';
+
+/**
+ * Date & Time Fields.
+ */
+export { default as DateTimeField } from './fields/dateTimeField/dateTimeField.js';
+export { default as DateField } from './fields/dateField/dateField.js';
+export { default as TimeField } from './fields/timeField/timeField.js';
+export { default as WeekField } from './fields/weekField/weekField.js';
+
+/**
+ * Options Fields.
+ */
+export { default as OptionsField } from './fields/optionsField/optionsField.js';
+export { default as FieldOption } from './fields/optionsField/fieldOption/fieldOption.js';
+
+export { default as RadioField } from './fields/radioField/radioField.js';
+export { default as RadioOption } from './fields/radioField/radioOption/radioOption.js';
+
+export { default as CheckboxesField } from './fields/checkboxesField/checkboxesField.js';
+export { default as CheckboxOption } from './fields/checkboxesField/checkboxOption/checkboxOption.js';
+
+export { default as SelectField } from './fields/selectField/selectField.js';
+export { default as SelectCombo } from './fields/selectCombo/selectCombo.js';
+export { default as SelectOption } from './fields/selectField/selectOption/selectOption.js';
+
+export { default as TagField } from './fields/tagField/tagField.js';
+export { default as TagOption } from './fields/tagField/components/tagOption/tagOption.js';
+
+/**
+ * Other Fields.
+ */
+
+export { default as ArrayField } from './fields/arrayField/arrayField.js';
+export { default as CheckboxField } from './fields/checkboxField/checkboxField.js';
+export { default as GroupField } from './fields/groupField/groupField.js';
+export { default as HiddenField } from './fields/hiddenField/hiddenField.js';
+export { default as SearchField } from './fields/searchField/searchField.js';
+
+/**
+ * File Fields.
+ */
+
+export { default as FileField } from './fields/fileField/fileField.js';
+export { default as FileFieldInput } from './fields/fileField/components/fileFieldInput/fileFieldInput.js';
+export { default as FileItem } from './fields/fileField/components/fileItem/fileItem.js';
+export { default as FileList } from './fields/fileField/components/fileList/fileList.js';
+export { default as ImageField } from './fields/imageField/imageField.js';
+export { default as ImageList } from './fields/imageField/components/imageList/imageList.js';
+export { default as ImageItem } from './fields/imageField/components/imageItem/imageItem.js';
--- src/index.js
+++ src/index.js
@@ -1,81 +1,42 @@
-import '@ungap/custom-elements';
-/**
- * Form.
- */
-export { default as FormComponent } from './components/form/form.js';
-export { default as SubmitButton } from './fields/submitButton/submitButton.js';
-export { default as FormTitle } from './components/form/components/formTitle/formTitle.js';
+export * from './exports.js';
+import * as forms from './exports.js';
 
-/**
- * Field.
- */
-export { default as Field } from './fields/field/field.js';
-export { default as FieldErrors } from './fields/field/components/fieldErrors/fieldErrors.js';
-export { default as FieldInput } from './fields/field/components/fieldInput/fieldInput.js';
-export { default as FieldLabel } from './fields/field/components/fieldLabel/fieldLabel.js';
-export { default as FieldDescription } from './fields/field/components/fieldDescription/fieldDescription.js';
-export { default as FieldFootnote } from './fields/field/components/fieldFootnote/fieldFootnote.js';
-export { default as FieldInputMask } from './fields/field/components/fieldInputMask/fieldInputMask.js';
+export const FieldsByType = {
+    array: forms.ArrayField,
+    checkbox: forms.CheckboxField,
+    checkboxes: forms.CheckboxesField,
+    color: forms.ColorField,
+    date: forms.DateField,
+    dateTime: forms.DateTimeField,
+    email: forms.EmailField,
+    file: forms.FileField,
+    group: forms.GroupField,
+    hidden: forms.HiddenField,
+    image: forms.ImageField,
+    month: forms.MonthField,
+    number: forms.NumberField,
+    options: forms.OptionsField,
+    password: forms.PasswordField,
+    radio: forms.RadioField,
+    range: forms.RangeField,
+    search: forms.SearchField,
+    select: forms.SelectField,
+    selectCombo: forms.SelectCombo,
+    tag: forms.TagField,
+    tel: forms.TelField,
+    text: forms.TextField,
+    textarea: forms.TextAreaField,
+    time: forms.TimeField,
+    url: forms.UrlField,
+    week: forms.WeekField
+};
 
-/**
- * Text Fields.
- */
-export { default as TextField } from './fields/textField/textField.js';
-export { default as PasswordField } from './fields/passwordField/passwordField.js';
-export { default as TextAreaField } from './fields/textAreaField/textAreaField.js';
-export { default as NumberField } from './fields/numberField/numberField.js';
-export { default as ColorField } from './fields/colorField/colorField.js';
-export { default as EmailField } from './fields/emailField/emailField.js';
-export { default as UrlField } from './fields/urlField/urlField.js';
-export { default as TelField } from './fields/telField/telField.js';
-export { default as RangeField } from './fields/rangeField/rangeField.js';
-export { default as MonthField } from './fields/monthField/monthField.js';
-
-/**
- * Date & Time Fields.
- */
-export { default as DateTimeField } from './fields/dateTimeField/dateTimeField.js';
-export { default as DateField } from './fields/dateField/dateField.js';
-export { default as TimeField } from './fields/timeField/timeField.js';
-export { default as WeekField } from './fields/weekField/weekField.js';
-
-/**
- * Options Fields.
- */
-export { default as OptionsField } from './fields/optionsField/optionsField.js';
-export { default as FieldOption } from './fields/optionsField/fieldOption/fieldOption.js';
-
-export { default as RadioField } from './fields/radioField/radioField.js';
-export { default as RadioOption } from './fields/radioField/radioOption/radioOption.js';
-
-export { default as CheckboxesField } from './fields/checkboxesField/checkboxesField.js';
-export { default as CheckboxOption } from './fields/checkboxesField/checkboxOption/checkboxOption.js';
-
-export { default as SelectField } from './fields/selectField/selectField.js';
-export { default as SelectCombo } from './fields/selectCombo/selectCombo.js';
-export { default as SelectOption } from './fields/selectField/selectOption/selectOption.js';
-
-export { default as TagField } from './fields/tagField/tagField.js';
-export { default as TagOption } from './fields/tagField/components/tagOption/tagOption.js';
-
-/**
- * Other Fields.
- */
-
-export { default as ArrayField } from './fields/arrayField/arrayField.js';
-export { default as CheckboxField } from './fields/checkboxField/checkboxField.js';
-export { default as GroupField } from './fields/groupField/groupField.js';
-export { default as HiddenField } from './fields/hiddenField/hiddenField.js';
-export { default as SearchField } from './fields/searchField/searchField.js';
-
-/**
- * File Fields.
- */
-
-export { default as FileField } from './fields/fileField/fileField.js';
-export { default as FileFieldInput } from './fields/fileField/components/fileFieldInput/fileFieldInput.js';
-export { default as FileItem } from './fields/fileField/components/fileItem/fileItem.js';
-export { default as FileList } from './fields/fileField/components/fileList/fileList.js';
-export { default as ImageField } from './fields/imageField/imageField.js';
-export { default as ImageList } from './fields/imageField/components/imageList/imageList.js';
-export { default as ImageItem } from './fields/imageField/components/imageItem/imageItem.js';
+(function bootstrap() {
+    if (typeof window === 'undefined') {
+        return;
+    }
+    if (typeof window.arpadroid === 'undefined') {
+        window.arpadroid = {};
+    }
+    window.arpadroid.forms = forms;
+})();
