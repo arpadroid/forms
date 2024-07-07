c0e4671093af7df7c879c1f731e1041aa21c29db
Andres Vaquero <web.andresvaquero@gmail.com>
2024-07-07 22:32:46 +1000

    adding lists project to external deps to avoid build warning

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,7 @@
 import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
-const { build } = getBuild('forms', 'uiComponent');
+const { build, appBuild, buildConfig } = getBuild('forms', 'uiComponent');
+const external = appBuild.external ?? [];
+if (buildConfig.slim) {
+    appBuild.external = [...external, '@arpadroid/lists'];
+}
 export default build;
